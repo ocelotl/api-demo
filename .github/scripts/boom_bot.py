83441776d95c1b5d1b45170a7635a4aa8b85f609
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-02-06 19:39:01 -0600

    WIP

--- .github/scripts/boom_bot.py
+++ .github/scripts/boom_bot.py
@@ -41,14 +41,14 @@ def check_pull_request(repo, pr_number):
 
     for file in files:
         if file.filename.endswith('.rst'):
-            print(file.raw_data)
-            content = file.raw_data.decode('utf-8')
-            lines = content.split('\n')
+            for line in [
+                line[1:] for line in file.patch.split("\n") if
+                line.startswith("+") and
+                len(line) > 1
+            ]:
 
-            for i, line in enumerate(lines, start=1):
-                if line.strip().startswith('BOOM'):
-                    comment = f"BOOM found in line {i}"
-                    pr.create_issue_comment(comment)
+                if line.strip().startswith('boom'):
+                    pr.create_issue_comment("boom found")
 
 
 if __name__ == "__main__":
@@ -57,3 +57,4 @@ if __name__ == "__main__":
     repo = g.get_repo('ocelotl/api-demo')
 
     check_pull_request(repo, int(environ.get("GITHUB_REF").split("/")[2]))
+    # check_pull_request(repo, 2)
